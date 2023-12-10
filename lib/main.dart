549a4a5238c9f383abcd9f5057773d0c9d97fd24
Dina Ahmed <denaahmed340@gmail.com>
2023-12-10 23:12:29 +0200

    create project create splash view

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'features/Splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashView();
+  }
+}
