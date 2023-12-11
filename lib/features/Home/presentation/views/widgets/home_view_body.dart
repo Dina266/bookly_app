2dfddbb49f4e0837feff6bb8e88e3348d2673b68
Dina Ahmed <denaahmed340@gmail.com>
2023-12-11 16:00:58 +0200

    add custom list view

--- lib/features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/features/Home/presentation/views/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
@@ -8,8 +9,32 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        CustomAppBar()
+        CustomAppBar(),
+        CustListViewItem(),
       ],
+    );
+  }
+}
+
+class CustListViewItem extends StatelessWidget {
+  const CustListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height *.3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          
+
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.blue,
+            image: DecorationImage(image: AssetImage(AssetsData.testImage))
+          ),
+        ),
+        ),
     );
   }
 }
