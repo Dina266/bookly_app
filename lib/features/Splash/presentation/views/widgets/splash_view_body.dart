58d191f3ecfd3419a52f266a69c9f63478766ff2
Dina Ahmed <denaahmed340@gmail.com>
2023-12-11 13:52:51 +0200

    refactor init state navigate to home view

--- lib/features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,5 +1,9 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import '../../../../Home/presentation/views/home_view.dart';
 import 'sliding_animation.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -16,16 +20,18 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(vsync: this , duration: const Duration(seconds: 1));
-    slidingAnimation = Tween<Offset>(begin: const Offset(0, 5),end: Offset.zero ).animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHome();
   }
+
+  
 
   @override
   void dispose() {
     animationController.dispose();
     super.dispose();
   }
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,4 +44,19 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
       ],
     );
   }
+
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(vsync: this , duration: const Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(begin: const Offset(0, 5),end: Offset.zero ).animate(animationController);
+    animationController.forward();
+
+  }
+  Future<Future<dynamic>?> navigateToHome() {
+    return Future.delayed(const Duration(seconds: 1) , () => Get.to(() => const HomeView() ,
+  transition: Transition.fade,
+  duration: kTransitionDuration
+  ));
+  }
+
 }
