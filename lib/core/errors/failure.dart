8c19815becc846505d5a6c102b7bacbe8e4cf3a6
Dina Ahmed <denaahmed340@gmail.com>
2024-01-30 22:47:12 +0200

    create server failure from dio

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,52 @@
-abstract class Failure{}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure{}
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+  factory ServerFailure.fromDioError(DioException dioExcep) {
+    switch (dioExcep.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection TimeOut with Api Server');
+
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send TimeOut with Api Server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Recieve TimeOut with Api Server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate with Api Server');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioExcep.response!.statusCode!, dioExcep.response);
+      case DioExceptionType.cancel:
+        return ServerFailure(' Request to Api Server was canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure(' Connection Error with Api Server');
+      case DioExceptionType.unknown:
+        if (dioExcep.message!.contains('SocketException')) {
+          return ServerFailure('No internet Connection');
+        } else {
+          return ServerFailure('UnExpected Error, please try again');
+        }
+      default :
+      return ServerFailure('Opps There was an error, please try again');
+
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found, please try again later');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server error, please try again later');
+    } else {
+      return ServerFailure('Opps There was an error, please try again');
+    }
+  }
+}
--- lib/features/Home/data/repos/home_repo_impl.dart
+++ lib/features/Home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/Home/data/model/book_model/book_model.dart';
 import 'package:bookly_app/features/Home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -15,15 +16,17 @@ class HomeRepoImpl implements HomeRepo {
           endPoint:
               'volumes?q=subject:Programming&Filtering=free-ebooks&Sorting=newest');
 
-      List <BookModel> books = [];
+      List<BookModel> books = [];
 
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
-        
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+    return left(ServerFailure(e.toString()));
     }
   }
 
