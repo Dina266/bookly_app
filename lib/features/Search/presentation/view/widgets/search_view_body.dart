2f81e42989efba55bece2cb8a0d36b43561d55ea
Dina Ahmed <denaahmed340@gmail.com>
2024-01-30 20:50:41 +0200

    custom search text field

--- lib/features/Search/presentation/view/widgets/search_view_body.dart
+++ lib/features/Search/presentation/view/widgets/search_view_body.dart
@@ -1,12 +1,16 @@
+import 'package:bookly_app/features/Search/presentation/view/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
 
-
 class SearchViewBody extends StatelessWidget {
-  const SearchViewBody ({super.key});
+  const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        children: [CustomSearchTextField()],
+      ),
+    );
   }
-  
 }
