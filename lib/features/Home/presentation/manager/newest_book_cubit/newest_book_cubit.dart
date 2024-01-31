70e45fb436dac84b341316aa484425bfbe99e1d2
Dina Ahmed <denaahmed340@gmail.com>
2024-01-31 19:58:50 +0200

    newest book cubit done

--- lib/features/Home/presentation/manager/newest_book_cubit/newest_book_cubit.dart
+++ lib/features/Home/presentation/manager/newest_book_cubit/newest_book_cubit.dart
@@ -2,8 +2,20 @@ import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/Home/data/model/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../data/repos/home_repo.dart';
+
 part 'newest_book_state.dart';
 
 class NewestBookCubitCubit extends Cubit<NewestBookCubitState> {
-  NewestBookCubitCubit() : super(NewestBookCubitInitial());
+  NewestBookCubitCubit(this.homeRepo) : super(NewestBookCubitInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> featchNewestBook() async {
+    emit(NewestBookCubitLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold((failure) => emit(NewestBookCubitFailure(failure.errMessage)),
+        (books) => emit(NewestBookCubitSuccess(books)));
+  }
 }
